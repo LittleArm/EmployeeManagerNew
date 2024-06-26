8e6393985611bab29baebc4bc7c05376cfb42969
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-06-26 09:40:35 +0700

    Update EmployeemanagerApplication.java

--- back-end/src/main/java/com/example/employeemanager/EmployeemanagerApplication.java
+++ back-end/src/main/java/com/example/employeemanager/EmployeemanagerApplication.java
@@ -2,8 +2,10 @@ package com.example.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EmployeemanagerApplication {
 
 	public static void main(String[] args) {

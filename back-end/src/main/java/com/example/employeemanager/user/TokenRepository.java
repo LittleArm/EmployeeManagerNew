2c6c427134651ded8fbbcacfc67ec10e012a669c
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-06-28 16:07:00 +0700

    Add Delete Method

--- back-end/src/main/java/com/example/employeemanager/user/TokenRepository.java
+++ back-end/src/main/java/com/example/employeemanager/user/TokenRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    void deleteByUserId(Long userId);
+
     Optional<Token> findByToken(String token);
 }

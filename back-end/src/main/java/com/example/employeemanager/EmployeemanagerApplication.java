3108e6f903a098f7c9dccad46092e5e3b1d60afd
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-06-28 11:46:11 +0700

    check if USER exists in RoleRepo

--- back-end/src/main/java/com/example/employeemanager/EmployeemanagerApplication.java
+++ back-end/src/main/java/com/example/employeemanager/EmployeemanagerApplication.java
@@ -1,15 +1,32 @@
 package com.example.employeemanager;
 
+import com.example.employeemanager.role.Role;
+import com.example.employeemanager.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class EmployeemanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
 	}
 
 }

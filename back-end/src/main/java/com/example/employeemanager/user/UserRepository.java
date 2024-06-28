f1561ec56e5913b0492c345459effce55d3cf444
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-06-28 11:44:24 +0700

    Update UserRepository.java

--- back-end/src/main/java/com/example/employeemanager/user/UserRepository.java
+++ back-end/src/main/java/com/example/employeemanager/user/UserRepository.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    void deleteUserByEmail(String email);
+    void deleteUserById(Long id);
 
     Optional<User> findByEmail(String email);
 }

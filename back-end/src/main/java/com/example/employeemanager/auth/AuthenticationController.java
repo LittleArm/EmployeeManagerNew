57f919c4e35c07192a3a3d3aa4f7df41d2cc8a71
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-06-28 17:08:11 +0700

    Update AuthenticationController.java

--- back-end/src/main/java/com/example/employeemanager/auth/AuthenticationController.java
+++ back-end/src/main/java/com/example/employeemanager/auth/AuthenticationController.java
@@ -6,11 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
@@ -34,5 +30,12 @@ public class AuthenticationController {
             @RequestBody @Valid AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/activate-account")
+    public void confirm(
+            @RequestParam String token
+    ) throws MessagingException {
+        service.activateAccount(token);
     }
 }

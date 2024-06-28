b51435fbfb074735d3473cefa3a9bbb373536444
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-06-28 16:07:52 +0700

    Add Manage Employee Methods

---
+++ back-end/src/main/java/com/example/employeemanager/manage/ManageController.java
@@ -0,0 +1,42 @@
+package com.example.employeemanager.manage;
+
+import com.example.employeemanager.user.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("employee")
+public class ManageController {
+    private final ManageService manageService;
+
+    public ManageController(ManageService manageService) {
+        this.manageService = manageService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllEmployees () {
+        List<User> employees = manageService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{email}")
+    public ResponseEntity<User> getEmployeeByEmail (@PathVariable("email") String email) {
+        User employee = manageService.findEmployee(email);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<User> updateEmployee(@RequestBody User employee) {
+        User updateEmployee = manageService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
+        manageService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ back-end/src/main/java/com/example/employeemanager/manage/ManageService.java
@@ -0,0 +1,53 @@
+package com.example.employeemanager.manage;
+
+import com.example.employeemanager.user.TokenRepository;
+import com.example.employeemanager.user.User;
+import com.example.employeemanager.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class ManageService {
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+
+    @Autowired
+    public ManageService(UserRepository userRepository, TokenRepository tokenRepository) {
+        this.userRepository = userRepository;
+        this.tokenRepository = tokenRepository;
+    }
+
+    public User addEmployee(User user) {
+        user.setEmployeeCode(UUID.randomUUID().toString());
+        return userRepository.save(user);
+    }
+
+    public List<User> findAllEmployees() {
+        return userRepository.findAll();
+    }
+
+    public User updateEmployee(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findEmployee(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User has email " + email + " was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        if (!userRepository.existsById(id)){
+            throw new UserNotFoundException("User not found with id " + id);
+        }
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found with id " + id));
+        user.getRoles().clear();
+        userRepository.save(user);
+        tokenRepository.deleteByUserId(id);
+        userRepository.deleteUserById(id);
+    }
+}
